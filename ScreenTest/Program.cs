678659f81bc92c99ffc6ac86d1bd3fc86452bc7b
Kyle Givler <xvegan88x@gmail.com>
2021-08-25 19:08:58 -0400

    Initial Demo

---
+++ ScreenTest/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * 
+ * This is where I would put the MIT license!
+ */
+
+using FFMpegCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ScreenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            for (int i = 0; i < 30; i++)
+            {
+                var bounds = new Rectangle();
+                bounds = Screen.PrimaryScreen.Bounds;
+                ScreenHelper.TakeAndSave(@".\images\", $"{DateTime.Now.Ticks}-shot.png", bounds, ImageFormat.Png);
+                Thread.Sleep(3 * 1000);
+            }
+
+            WriteVideo();
+        }
+
+        private static void WriteVideo()
+        {
+            var images = Directory.GetFiles(".\\images", "*png");
+
+            var imageInfo = new List<ImageInfo>();
+            foreach (var image in images)
+            {
+                imageInfo.Add(ImageInfo.FromPath(image));
+                imageInfo.Add(ImageInfo.FromPath(image));
+                imageInfo.Add(ImageInfo.FromPath(image));
+            }
+
+            FFMpeg.JoinImageSequence("test.mp4", 30, imageInfo.ToArray());
+        }
+    }
+}
---
+++ ScreenTest/ScreenHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ScreenTest
+{
+    public class ScreenHelper
+    {
+        private static Bitmap CopyFromScreen(Rectangle bounds)
+        {
+            try
+            {
+                var image = new Bitmap(bounds.Width, bounds.Height);
+                using var graphics = Graphics.FromImage(image);
+                graphics.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                return image;
+            }
+            catch (Win32Exception)
+            {//When screen saver is active
+                return null;
+            }
+        }
+
+        public static Image Take(Rectangle bounds)
+        {
+            return CopyFromScreen(bounds);
+
+        }
+
+        public static byte[] TakeAsByteArray(Rectangle bounds)
+        {
+            using var image = CopyFromScreen(bounds);
+            using var ms = new MemoryStream();
+            image.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+
+        public static void TakeAndSave(string path, string fileName, Rectangle bounds, ImageFormat imageFormat)
+        {
+            using var image = CopyFromScreen(bounds);
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            image.Save(path + fileName, imageFormat);
+        }
+    }
+}
